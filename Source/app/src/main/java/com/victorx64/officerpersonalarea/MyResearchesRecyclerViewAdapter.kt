17ed0a35676789daa45253830821d824a29a0dc4
unknown <Gyallahorn@gmail.com>
2019-07-26 16:03:50 +0900

    added expandable cardview to other fragment still needed little fixes

--- Source/app/src/main/java/com/victorx64/officerpersonalarea/MyResearchesRecyclerViewAdapter.kt
+++ Source/app/src/main/java/com/victorx64/officerpersonalarea/MyResearchesRecyclerViewAdapter.kt
@@ -1,13 +1,19 @@
 package com.victorx64.officerpersonalarea
 
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.healthyworker.MyResearcheslFragment
+import com.victorx64.officerpersonalarea.MyDoctorRecyclerViewAdapter.ViewHolder.Companion.collapse
+import com.victorx64.officerpersonalarea.MyDoctorRecyclerViewAdapter.ViewHolder.Companion.expand
 
 class MyResearchesRecyclerViewAdapter(private val mValues: List<ResearchesContent.ResearchItem>, private val mListener: MyResearcheslFragment.OnListFragmentInteractionListener?) : RecyclerView.Adapter<MyResearchesRecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyResearchesRecyclerViewAdapter.ViewHolder {
@@ -31,21 +37,55 @@ class MyResearchesRecyclerViewAdapter(private val mValues: List<ResearchesConten
        }
     }
 
-    class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+    class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView),View.OnClickListener {
+
+        fun Visibility_mAbout(){
+            mAbout.visibility = VISIBLE
+        }
+
+
+
         val mTitle: TextView
         val mImage: ImageView
         val mCabinet: TextView
         var mItem: ResearchesContent.ResearchItem? = null
         var mCardView: CardView
+        val mAbout : TextView
+        val mExpandButton : ImageButton
+        val mCollapseButton : ImageButton
 
         init {
             mTitle = mView.findViewById(R.id.researches_title)
             mImage = mView.findViewById(R.id.researches_image)
             mCabinet = mView.findViewById(R.id.floor_and_cabinet)
             mCardView = mView.findViewById(R.id.Researches_cardView)
+            mAbout = mView.findViewById(R.id.res_item_about)
+            mExpandButton = mView.findViewById(R.id.res_expand_button)
+            mExpandButton.setOnClickListener(this)
+            mCollapseButton = mView.findViewById(R.id.res_collapse_button)
+            mCollapseButton.setOnClickListener(this)
+
+        }
+        override fun onClick(v: View?) {
+            when (v?.id){
+                R.id.res_expand_button ->{
+                    expand(mCardView)
+                    val handler = Handler()
+                    handler.postDelayed({Visibility_mAbout()},400)
+                    mExpandButton.visibility = INVISIBLE
+                    mCollapseButton.visibility = VISIBLE
+                }
+                R.id.res_collapse_button ->{
+                    mAbout.visibility = INVISIBLE
+                    mExpandButton.visibility= VISIBLE
+                    mCollapseButton.visibility  = INVISIBLE
+                    collapse(mCardView)
+                }
+            }
         }
 
     }
+
 
 
 }
